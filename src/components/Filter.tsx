36db5a7c8612e0300c73b241453eb15a7e05ee19
Elix Neto <neto.elix@outlook.com>
2023-11-16 08:45:36 -0300

    Adicionando testes a aplicação e finalizando requisito 6

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -13,6 +13,16 @@ function Filter() {
   const [searchText, useText] = useState('');
   const [value, setValue] = useState(0);
   const [finalArray, setFinalArray] = useState([]);
+  const [filterArray, setFilteredArray] = useState([{}]);
+  const [filters, setFilters] = useState([]);
+  const [columnOptions, setColumnOptions] = useState([
+    'population',
+    'orbital_period',
+    'diameter',
+    'rotation_period',
+    'surface_water',
+  ]);
+
   const keyItem = 'name';
 
   const filteredArray = useFilter(searchText, keyItem, planets);
@@ -46,6 +56,34 @@ function Filter() {
 
   function HandleClickButton() {
     setFinalArray(secondFilteredArray);
+    setFilteredArray([{ itemOperator, itemOption, value }]);
+    setFilters((prevFilters) => [...prevFilters, { itemOperator, itemOption, value }]);
+    setColumnOptions((prevOptions) => prevOptions
+      .filter((option) => option !== itemOption));
+  }
+
+  function handleDelete(index: number) {
+    setFilters((prevFilters) => {
+      const filterToDelete = prevFilters[index];
+
+      setColumnOptions((prevOptions) => [...prevOptions, filterToDelete.itemOption]);
+
+      return prevFilters.filter((filter, i) => i !== index);
+    });
+  }
+
+  function handleDeleteAll() {
+    setFilters([]);
+
+    setColumnOptions([
+      'population',
+      'orbital_period',
+      'diameter',
+      'rotation_period',
+      'surface_water',
+    ]);
+    setFilters([]);
+    setFinalArray(planets);
   }
 
   return (
@@ -68,11 +106,9 @@ function Filter() {
             onChange={ HandleColun }
             data-testid="column-filter"
           >
-            <option value="population">population</option>
-            <option value="orbital_period">orbital_period</option>
-            <option value="diameter">diameter</option>
-            <option value="rotation_period">rotation_period</option>
-            <option value="surface_water">surface_water</option>
+            {columnOptions.map((option) => (
+              <option key={ option } value={ option }>{option}</option>
+            ))}
           </select>
         </div>
         <div className="filter__size">
@@ -106,6 +142,24 @@ function Filter() {
         >
           FILTRAR
         </button>
+        <div>
+          {filters.map((filter, index) => (
+            <div key={ index } data-testid="filter">
+              <p>{`${filter.itemOption} ${filter.itemOperator} ${filter.value}`}</p>
+              <button onClick={ () => handleDelete(index) }>Delete</button>
+            </div>
+          ))}
+          {/* {filters.length === 0 ? <p>Nenhum filtro aplicado</p>
+            : <div>
+              <button
+                data-testid="button-remove-filters"
+                onClick={ handleDeleteAll }
+              >
+                Remover todas filtragens
+
+              </button>
+              </div>} */}
+        </div>
       </div>
       <Table filteredArray={ finalArray as Planet[] } />
     </div>
--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,9 +1,54 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 import App from '../App';
 
-test('I am your test', () => {
+test('Teste se os itens principais são renderizados', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Hello, App!/i);
-  expect(linkElement).toBeInTheDocument();
+  const colum = screen.getByText(/coluna/i);
+  expect(colum).toBeInTheDocument();
+  const operator = screen.getByText(/operador/i);
+  expect(operator).toBeInTheDocument();
+  const value = screen.getByText(/valor/i);
+  expect(value).toBeInTheDocument();
+  const filtrar = screen.getByText(/filtrar/i);
+  expect(filtrar).toBeInTheDocument();
 });
+test('Teste se os itens da API estão carregando', async () => {
+  render(<App />);
+  const loading = screen.getByText(/Loading.../i);
+  expect(loading).toBeInTheDocument();
+
+  await waitFor(() => {
+    expect(loading).not.toBeInTheDocument();
+  }, { timeout: 5000 });
+  const data = await screen.findByText(/Alderaan/i);
+  expect(data).toBeInTheDocument();
+
+} );
+
+test('Teste se os botões de filtro estão funcionando', async () => {
+  render(<App />);
+  const loading = screen.getByText(/Loading.../i);
+  expect(loading).toBeInTheDocument();
+
+  await waitFor(() => {
+    expect(loading).not.toBeInTheDocument();
+  }, { timeout: 5000 });
+  const data = await screen.findByText(/Alderaan/i);
+  expect(data).toBeInTheDocument();
+
+  const coluna = screen.getByTestId('column-filter');
+  expect(coluna).toBeInTheDocument();
+  const operador = screen.getByTestId('comparison-filter');
+  expect(operador).toBeInTheDocument();
+  const valor = screen.getByTestId('value-filter');
+  expect(valor).toBeInTheDocument();
+  const filtrarButton = screen.getByTestId('button-filter');
+  expect(filtrarButton).toBeInTheDocument();
+  fireEvent.change(coluna, { target: { value: 'orbital_period' } });
+  fireEvent.change(operador, { target: { value: 'maior que' } });
+  fireEvent.change(valor, { target: { value: 500 } });
+  fireEvent.click(filtrarButton);
+  const data2 = await screen.findByText(/Yavin IV/i); 
+  expect(data2).toBeInTheDocument();
+} );
